c4da6e486c4c865c84e881f4fd1cabd9e3598a3c
AnmolMajithia <rush2asm@gmail.com>
2021-07-13 16:03:43 +0530

    ORM and Table Intergration in Admin

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'AnmolM',
-        'title': 'Blog Post 1',
-        'content': 'First Post Content',
-        'date_posted': 'June 13, 2021'
-    },
-    {
-        'author': 'Dummy1',
-        'title': 'Blog Post 2',
-        'content': 'Second Post Content',
-        'date_posted': 'June 14, 2021'
-    },
-]
-
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
